6ab8784ca5ed6c6702cdbf9b85a98c1c4438d61b
Paing Paing <tonypaingpaing3000@gmail.com>
2025-10-20 19:03:44 +0630

    Version 154

--- admin/admin_config.php
+++ admin/admin_config.php
@@ -3,9 +3,5 @@
 // Admin Configuration File
 // ==============================
 
-// Admin email
 $admin_email = "paingpaing@gmail.com";
-
-// Admin hashed password (for example only)
-// The original password for this hash is: skibidi
 $admin_hashed_password = '$2y$10$5Z0SR/YoeDVJJQfvJB0WNeWayY6BzjLZ7uKx2jai06Lyp.hgJgFia';
--- admin/test_verify.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/admin_config.php";
-
-$test = 'skibidi';
-$ok = password_verify($test, $admin_hashed_password);
-
-echo "Testing password: " . htmlspecialchars($test) . "<br>";
-echo "Hash: " . htmlspecialchars($admin_hashed_password) . "<br>";
-echo "password_verify result: " . ($ok ? 'MATCH' : 'NO MATCH');
