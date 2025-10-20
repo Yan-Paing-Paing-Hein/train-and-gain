d4058f0f2a255b33828d223e7b51285eb2814094
Paing Paing <tonypaingpaing3000@gmail.com>
2025-10-20 19:22:31 +0630

    Version 156

--- admin/admin_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ==============================
-// Admin Configuration File
-// ==============================
-
-$admin_email = "paingpaing@gmail.com";
-$admin_hashed_password = '$2y$10$klgYKlvrQVdx3C6FcHYA3uk.W0N3SACNBberafQpSgkPerh.6AYoO';
